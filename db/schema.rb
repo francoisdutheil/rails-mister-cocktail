227c80a461434e2bbd9af53e40886c24ce4a2971
francois dutheil <frdutheil@yahoo.fr>
2019-02-14 18:23:16 +0100

    first day cocktail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_145602) do
+ActiveRecord::Schema.define(version: 2019_02_14_114511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,4 @@ ActiveRecord::Schema.define(version: 2019_02_14_145602) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_foreign_key "doses", "cocktails"
-  add_foreign_key "doses", "ingredients"
 end
